4cd3c1f4abd0d96971b570625731fa7bc82ffd29
desarrolloyc <desarrolloyc@desarrolloyc-MacBookPro>
2018-06-26 00:00:49 -0500

    Se añaden gráficas de barras para la categorías areas, profesión y jorn.

--- harmony/src/main/java/gui/portal/climalaboral/ResultadosBean.java
+++ harmony/src/main/java/gui/portal/climalaboral/ResultadosBean.java
@@ -1,8 +1,13 @@
 package gui.portal.climalaboral;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -32,8 +37,11 @@ public class ResultadosBean
 	private BarChartModel	modeloSecSeleccionada;
 
 	private PieChartModel	modeloTotalesArea;
+	private BarChartModel	modeloBarTotalesArea;
 	private PieChartModel	modeloTotalesProfesion;
+	private BarChartModel	modeloBarTotalesProfesion;
 	private PieChartModel	modeloTotalesJornada;
+	private BarChartModel	modeloBarTotalesJornada;
 
 	private Encuesta		encuesta;
 	private Seccion			secSeleccionada;
@@ -48,10 +56,14 @@ public class ResultadosBean
 	private Profesion		profesionSelec;
 	private Jornada			jornadaSelec;
 	private int				totalRegistros;
+	private String			seriesColors;
 
 	public ResultadosBean()
 	{
-
+		this.seriesColors = "58BA27,FFCC33,2db5ff,F52F2F,A30303,bcd2dd,ceeeff,105a82,707d84,8e6e68,"
+				+ "b4eab4,515b51,091e09,00ff00,00ffe9,002afc,b200ff,ff00dc,fc0093,f2002c,"
+				+ "efb8c2,baf298,a1ef97,8ce2af,8aeacf,7fd7e2,76a7db,9068cc,af63c6,e26897,"
+				+ "6b382f,6d512e,68632c,546028,415b25,276338,2a7064,26576d,253170,46216d ";
 	}
 
 	@PostConstruct
@@ -67,6 +79,9 @@ public class ResultadosBean
 		getResultadosFromBD();
 		createBarModel();
 		createPieModel();
+		createBarModelProfesion();
+		createBarModelJornada();
+		createBarModelArea();
 	}
 
 	public void getResultadosFromBD()
@@ -81,7 +96,8 @@ public class ResultadosBean
 	{
 
 		modeloGrafica = initBarModel();
-		modeloGrafica.setAnimate(true);
+		modeloGrafica.setSeriesColors(this.seriesColors);
+		//		modeloGrafica.setAnimate(true);
 
 		modeloGrafica.setTitle("Resultados Generales por Categoría");
 		modeloGrafica.setLegendPosition("ne");
@@ -96,30 +112,135 @@ public class ResultadosBean
 
 	}
 
+	private void createBarModelProfesion()
+	{
+
+		modeloBarTotalesProfesion = initBarModelProfesion();
+		modeloBarTotalesProfesion.setSeriesColors(this.seriesColors);
+		//		modeloBarTotalesProfesion.setAnimate(true);
+
+		modeloBarTotalesProfesion.setTitle("Totales por Profesión");
+		modeloBarTotalesProfesion.setLegendPosition("ne");
+
+		Axis xAxis = modeloBarTotalesProfesion.getAxis(AxisType.X);
+		xAxis.setLabel("Profesión");
+
+		Axis yAxis = modeloBarTotalesProfesion.getAxis(AxisType.Y);
+		yAxis.setLabel("Encuestados");
+		yAxis.setMin(0);
+
+		Map<String, Integer> mapTotalesProfesion = this.encuesta.getTotalesClasificacion().get(1);
+
+		Map<String, Integer> result = mapTotalesProfesion.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		Entry<String, Integer> entry = result.entrySet().iterator().next();
+		int value = entry.getValue();
+
+		yAxis.setMax(value);
+
+	}
+
+	private void createBarModelArea()
+	{
+
+		modeloBarTotalesArea = initBarModelArea();
+		modeloBarTotalesArea.setSeriesColors(this.seriesColors);
+		//		modeloBarTotalesArea.setAnimate(true);
+
+		modeloBarTotalesArea.setTitle("Totales por Área");
+		modeloBarTotalesArea.setLegendPosition("ne");
+
+		Axis xAxis = modeloBarTotalesArea.getAxis(AxisType.X);
+		xAxis.setLabel("Área");
+
+		Axis yAxis = modeloBarTotalesArea.getAxis(AxisType.Y);
+		yAxis.setLabel("Encuestados");
+		yAxis.setMin(0);
+
+		Map<String, Integer> mapTotalesAreas = this.encuesta.getTotalesClasificacion().get(0);
+
+		Map<String, Integer> result = mapTotalesAreas.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		Entry<String, Integer> entry = result.entrySet().iterator().next();
+		int value = entry.getValue();
+
+		yAxis.setMax(value);
+
+	}
+
+	private void createBarModelJornada()
+	{
+
+		modeloBarTotalesJornada = initBarModelJornada();
+		modeloBarTotalesJornada.setSeriesColors(this.seriesColors);
+		//		modeloBarTotalesJornada.setAnimate(true);
+
+		modeloBarTotalesJornada.setTitle("Totales por Jornada");
+		modeloBarTotalesJornada.setLegendPosition("ne");
+
+		Axis xAxis = modeloBarTotalesJornada.getAxis(AxisType.X);
+		xAxis.setLabel("Jornada");
+
+		Axis yAxis = modeloBarTotalesJornada.getAxis(AxisType.Y);
+		yAxis.setLabel("Encuestados");
+		yAxis.setMin(0);
+
+		Map<String, Integer> mapTotalesJornada = this.encuesta.getTotalesClasificacion().get(2);
+
+		Map<String, Integer> result = mapTotalesJornada.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		Entry<String, Integer> entry = result.entrySet().iterator().next();
+		int value = entry.getValue();
+
+		yAxis.setMax(value);
+
+	}
+
 	private void createPieModel()
 	{
 
 		this.modeloTotalesArea = new PieChartModel();
+		this.modeloTotalesArea.setSeriesColors(this.seriesColors);
 		this.modeloTotalesProfesion = new PieChartModel();
+		this.modeloTotalesProfesion.setSeriesColors(this.seriesColors);
 		this.modeloTotalesJornada = new PieChartModel();
+		this.modeloTotalesJornada.setSeriesColors(this.seriesColors);
 
 		Map<String, Integer> mapTotalesAreas = this.encuesta.getTotalesClasificacion().get(0);
 		Map<String, Integer> mapTotalesProfesion = this.encuesta.getTotalesClasificacion().get(1);
 		Map<String, Integer> mapTotalesJornada = this.encuesta.getTotalesClasificacion().get(2);
 
-		mapTotalesAreas.forEach((k, v) ->
+		Map<String, Integer> resultArea = mapTotalesAreas.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		Map<String, Integer> resultProfesion = mapTotalesProfesion.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		Map<String, Integer> resultJornada = mapTotalesJornada.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		resultArea.forEach((k, v) ->
 		{
 			this.modeloTotalesArea.set(k, v);
 
 		});
 
-		mapTotalesProfesion.forEach((k, v) ->
+		resultProfesion.forEach((k, v) ->
 		{
 			this.modeloTotalesProfesion.set(k, v);
 
 		});
 
-		mapTotalesJornada.forEach((k, v) ->
+		resultJornada.forEach((k, v) ->
 		{
 			this.modeloTotalesJornada.set(k, v);
 
@@ -214,6 +335,78 @@ public class ResultadosBean
 			}
 
 		}
+
+		return model;
+
+	}
+
+	private BarChartModel initBarModelProfesion()
+	{
+		BarChartModel model = new BarChartModel();
+
+		Map<String, Integer> mapTotalesProfesion = this.encuesta.getTotalesClasificacion().get(1);
+
+		Map<String, Integer> result = mapTotalesProfesion.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		result.forEach((k, v) ->
+		{
+			ChartSeries secChart = new ChartSeries();
+			secChart.setLabel(k);
+
+			secChart.set(" ", v);
+			model.addSeries(secChart);
+
+		});
+
+		return model;
+
+	}
+
+	private BarChartModel initBarModelArea()
+	{
+		BarChartModel model = new BarChartModel();
+
+		Map<String, Integer> mapTotalesAreas = this.encuesta.getTotalesClasificacion().get(0);
+
+		Map<String, Integer> result = mapTotalesAreas.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		result.forEach((k, v) ->
+		{
+			ChartSeries secChart = new ChartSeries();
+			secChart.setLabel(k);
+
+			secChart.set(" ", v);
+			model.addSeries(secChart);
+
+		});
+
+		return model;
+
+	}
+
+	private BarChartModel initBarModelJornada()
+	{
+		BarChartModel model = new BarChartModel();
+
+		Map<String, Integer> mapTotalesJornada = this.encuesta.getTotalesClasificacion().get(2);
+
+		Map<String, Integer> result = mapTotalesJornada.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).collect(Collectors.toMap(
+						Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+		result.forEach((k, v) ->
+		{
+			ChartSeries secChart = new ChartSeries();
+			secChart.setLabel(k);
+
+			secChart.set(" ", v);
+			model.addSeries(secChart);
+
+		});
 
 		return model;
 
@@ -436,6 +629,36 @@ public class ResultadosBean
 	public void setModeloTotalesJornada(PieChartModel modeloTotalesJornada)
 	{
 		this.modeloTotalesJornada = modeloTotalesJornada;
+	}
+
+	public BarChartModel getModeloBarTotalesProfesion()
+	{
+		return modeloBarTotalesProfesion;
+	}
+
+	public void setModeloBarTotalesProfesion(BarChartModel modeloBarTotalesProfesion)
+	{
+		this.modeloBarTotalesProfesion = modeloBarTotalesProfesion;
+	}
+
+	public BarChartModel getModeloBarTotalesArea()
+	{
+		return modeloBarTotalesArea;
+	}
+
+	public void setModeloBarTotalesArea(BarChartModel modeloBarTotalesArea)
+	{
+		this.modeloBarTotalesArea = modeloBarTotalesArea;
+	}
+
+	public BarChartModel getModeloBarTotalesJornada()
+	{
+		return modeloBarTotalesJornada;
+	}
+
+	public void setModeloBarTotalesJornada(BarChartModel modeloBarTotalesJornada)
+	{
+		this.modeloBarTotalesJornada = modeloBarTotalesJornada;
 	}
 
 }
