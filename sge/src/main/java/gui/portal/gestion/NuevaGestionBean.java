bf4f51a1949d5ca3c4d782fe292a936720c62a86
desarrolloyc <desarrolloyc@desarrolloyc-MacBookPro>
2018-07-19 10:14:00 -0500

    Nuevos filtros

--- sge/src/main/java/gui/portal/gestion/NuevaGestionBean.java
+++ sge/src/main/java/gui/portal/gestion/NuevaGestionBean.java
@@ -516,6 +516,7 @@ public class NuevaGestionBean
 				}
 				else
 				{
+					
 					//Se añade un comentario a la tarea indicando que la tarea o gestión fue editada
 					wUsuario.getTareaWunderlist(this.gestion.getIdTareaWunderlist());
 
